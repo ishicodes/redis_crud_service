a8c7875aa265631e2f603ae77a88ea51bf846c29
ishicodes <sharda.bansal71@gmail.com>
2025-02-18 18:28:38 +0530

    Update app.py for ports and localhost binding

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def delete_incident(key):
     return jsonify({"message": f"Key '{key}' deleted successfully"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=10000)
